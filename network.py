2c08b29788edbb1b708acf41a793eb11d9221615
Ryan-Andrade <94945327+Ryan-Andrade@users.noreply.github.com>
2025-09-08 22:34:57 -0700

    I think I got the dot product comment to where I like it.

--- network.py
+++ network.py
@@ -74,13 +74,13 @@ def compute_gradients (self, input_layer, target_value):
     zs = []
     
     for b, w in zip( self.biases , self.weights ):
-        # Each row (neuron) of the weight matrix (layer connections) lines up its columns (weights) 
+        # Dot product takes each row (neuron) of the weight matrix (layer connections) lines up its columns (weights) 
         # with the one input/activation vector (column) and multiplies each weight by its corresponding 
-        # input/activation, and then sums those products to result in a single number for that row (neuron). This 
+        # input/activation, and then sums those products to result in a single value for that row (neuron). It 
         # transforms the weight matrix into a vector (one entry per row/neuron). After that, the bias vector
-        # (column) is added to the result, calculating the pre-activation (z) values for the layer (column vector).
+        # (column) is added to the result of the dot product, calculating the pre-activation (z) values for the layer (column vector).
         # Numpy's vectorization allows each calculation to be performed on all rows simultaneously in parallel.
-        z = np.dot(w, activation )+b
+        z = np.dot(w, activation)+b
         zs.append(z)
         activation = sigmoid (z)
         activations.append(activation)
